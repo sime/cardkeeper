f0c33116f3c7f168eba6e810edc6e4f05eb5b530
Evan Brass <evan-brass@protonmail.com>
2022-02-20 12:17:50 -0800

    Add error message for on

--- js/templating/expressions.mjs
+++ js/templating/expressions.mjs
@@ -1,6 +1,7 @@
 // Event Handling
 export function on(event, handler, options = {}) {
-	return (target, signal) => {
-		target.addEventListener(event, handler, { signal, ...options });
+	return (node, signal) => {
+		if (!node?.addEventListener) throw new Error("on only works on EventTargets");
+		node.addEventListener(event, handler, { signal, ...options });
 	};
 }
